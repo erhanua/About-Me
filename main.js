b68a8d5bba40b8074caba19947010b022f181e5f
erhanua <106486822+erhanua@users.noreply.github.com>
2023-09-21 11:29:09 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,34 +1,81 @@
 function startGame() {
   // Personalized welcome message
   let userName = prompt("What is your name?");
-  alert("Welcome to our site" +userName);
+  alert("Welcome to our site, " + userName + "!");
 
   // Questions and their correct answers
   const questions = [
-    "Is JavaScript a programming language? (yes/no)",
-    "Is CSS a backend technology? (yes/no)",
-    "Is HTML used for structuring web content? (yes/no)",
-    "Is Python a frontend technology? (yes/no)",
-    "Is Node.js runtime built on Chromes V8 JavaScript engine? (yes/no)",
+    "Which property in CSS is used to set the background color of an element? (background-color/color/border-color)",
+    "Which HTML element is used to link an external CSS file? (link/script/style)",
+    "Which JavaScript method is used to select an element by its ID? (getElementById/querySelector/selectElement)",
+    "Which CSS property controls the text size? (font-size/text-size/font-weight)",
+    "In HTML, which attribute is used to specify an input field is required? (required/mandatory/needed)",
+    "Which of the following is not a CSS unit for font sizes? (em/rem/pxl)",
+    "Guess the year when HTML5 was officially finalized and published:",
   ];
-  const correctAnswers = ["yes", "no", "yes", "no", "yes"];
+
+  const correctAnswers = [
+    "background-color",
+    "link",
+    "getelementbyid",
+    "font-size",
+    "required",
+    "pxl",
+    "2014",
+  ];
+
+  let score = 0; // Track the user's correct answers
 
   // Ask questions and validate answers
   for (let i = 0; i < questions.length; i++) {
     let userAnswer = prompt(questions[i]).toLowerCase();
-    if (userAnswer === "y") userAnswer = "yes";
-    if (userAnswer === "n") userAnswer = "no";
+
+    // For the 7th question, give the user 4 attempts
+    if (i === 6) {
+      let attempts = 4; // Set the number of attempts to 4
+      while (attempts > 0) {
+        if (userAnswer === correctAnswers[i]) {
+          alert("Correct!");
+          score++;
+          break;
+        } else {
+          attempts--;
+          if (attempts > 0) {
+            if (Number(userAnswer) > Number(correctAnswers[i])) {
+              userAnswer = prompt(
+                "Too high! Try again! You have " +
+                  attempts +
+                  " attempts left. " +
+                  questions[i]
+              ).toLowerCase();
+            } else {
+              userAnswer = prompt(
+                "Too low! Try again! You have " +
+                  attempts +
+                  " attempts left. " +
+                  questions[i]
+              ).toLowerCase();
+            }
+          } else {
+            alert("Incorrect. The correct answer was: " + correctAnswers[i]);
+          }
+        }
+      }
+      continue;
+    }
 
     // Check if the answer is correct
     if (userAnswer === correctAnswers[i]) {
-      // console.log('Correct!'); // Uncomment for debugging
       alert("Correct!");
+      score++;
     } else {
-      // console.log('Incorrect.'); // Uncomment for debugging
       alert("Incorrect.");
     }
   }
 
-  // Final message
-  alert("Thanks for participating, "+ userName + "We hope to see you again.");
+  // Final messages
+  alert("You got " + score + "/7 questions correct.");
+  alert(
+    "Thanks for participating, " + userName + ". We hope to see you again."
+  );
 }
